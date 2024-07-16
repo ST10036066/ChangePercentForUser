9571cefefc820b50584a9c1d75b0d05d6fdf4b65
Azania Aria Ncube <ST10036066@vcconnect.edu.za>
2024-07-16 22:55:55 +0200

    Add project files.

---
+++ ChangePercentForUser/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace percentageCalculator
+{
+    public class Class1
+    {
+        //a method that recieve two numbers the mark
+
+        public double percent(double x, double y )
+        {
+            // Check if the inputs are valid
+          /*  if (x < 0 || y < 0 || y > 100)
+            {
+                throw new ArgumentException("Invalid input");
+            }
+          */
+            // Calculate the percentage using the formula
+            double result = (x / 100.0) * y;
+
+            // Return the result that the user expects 
+            return result;
+
+            
+        }
+    }
+}
---
+++ ChangePercentForUser/Program.cs
@@ -0,0 +1,37 @@
+﻿using percentageCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangePercentForUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name >>>");
+            string nn = Console.ReadLine();
+
+            Console.WriteLine("Please enter the student mark>>");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the percent (%) to increase the result by >>");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            //exception handling 
+            // Check if the inputs are valid
+            if (x < 0 || y < 0 || y > 100)
+            {
+                throw new ArgumentException("Invalid input");
+            }
+
+            //using the ddl 
+            Class1 cc = new Class1();
+            cc.percent(x, y);
+
+            Console.WriteLine("The new results for " + nn + cc);
+        }
+    }
+}
